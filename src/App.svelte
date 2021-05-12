ca7b64f1fff2c4cd9b3a52dab92d93986f8407ab
77Z <r2d2fan20@outlook.com>
2021-05-12 13:47:57 +0000

    init

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Content from "./shared/Content.svelte"
+</script>
+
+
+
+<Content>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia iure consequuntur excepturi minima? Iusto obcaecati debitis maxime eligendi, et unde? Ea totam enim eveniet id aspernatur pariatur aliquid. Illum autem maiores illo minima, similique numquam ab perspiciatis deleniti corrupti error molestiae architecto suscipit magnam magni expedita totam. Architecto dolor voluptatibus consectetur dignissimos officia officiis deserunt iure alias animi, ipsam iusto eos! Adipisci qui consectetur esse, est vitae saepe assumenda dicta maiores iure inventore id aspernatur quasi animi a exercitationem dolores corrupti beatae, obcaecati distinctio tempore! Totam assumenda consequatur a voluptatibus recusandae neque velit, reiciendis iure ad, asperiores, aperiam est?
+</Content>
---
+++ src/shared/Header.svelte
@@ -0,0 +1,15 @@
+<div class="header">
+    Game Warden
+</div>
+
+<style>
+    .header {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 2rem;
+        font-size: 3em;
+        text-align: center;
+    }
+</style>
