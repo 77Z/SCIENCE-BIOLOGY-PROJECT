d1d371811863a6cbf84292971485b0a2b78df98b
Vince R <r2d2fan20@outlook.com>
2021-05-16 21:57:31 -0500

    Woohoo

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,137 @@
 <script>
-	import Content from "./shared/Content.svelte"
+	import Content from './shared/Content.svelte';
 </script>
 
-
+<div class="title">
+	<h1>Game Warden</h1>
+	&nbsp;By. Vince
+</div>
 
 <Content>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia iure consequuntur excepturi minima? Iusto obcaecati debitis maxime eligendi, et unde? Ea totam enim eveniet id aspernatur pariatur aliquid. Illum autem maiores illo minima, similique numquam ab perspiciatis deleniti corrupti error molestiae architecto suscipit magnam magni expedita totam. Architecto dolor voluptatibus consectetur dignissimos officia officiis deserunt iure alias animi, ipsam iusto eos! Adipisci qui consectetur esse, est vitae saepe assumenda dicta maiores iure inventore id aspernatur quasi animi a exercitationem dolores corrupti beatae, obcaecati distinctio tempore! Totam assumenda consequatur a voluptatibus recusandae neque velit, reiciendis iure ad, asperiores, aperiam est?
+	<p>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You’ve got questions,
+		and I’m here to answer ‘em! First off, what’s a Game Warden? Game
+		Wardens, also known as Conservation Wardens, have a vast and wide
+		variety of skills that help them with their duties. Ok, so what does a
+		Game Warden do? They spend most of their time on field almost acting
+		like a police officer of nature. Sometimes you will hear them as Game
+		and Fish Wardens, because they regulate fish too, and if you didn’t
+		know, game, refers to hunting because they regulate that too. I’ve heard
+		some sources refer to Game Wardens as detectives because they have to
+		hunt down and capture poachers, and depending on what kind of poaching
+		is happening, it can be really hard to determine if someone is poaching,
+		or if they’re genuinely hunting. Because you can’t check everything.
+		Again, it might be easier to find people hunting with laser sights(which
+		is not allowed) than it might be to find people with 100% valid hunting
+		or fishing licences, or if they’re using a legal firearm.
+	</p>
+	<img src="/warden.png" alt="Game Warden" />
+	<p>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enough with the
+		questions, let’s talk about how and why you might want to become a game
+		warden. As said before, they do tons of different things, and you can’t
+		just be anyone looking to become a Game Warden, there’s some pretty
+		strict requirements. First off, you need to be at least 21 years old
+		because you’ll be dealing with firearms and similar things, you’re
+		basically a police officer of nature, so you never know what you’re
+		going to come across. You also have to be physically fit, you’ll be
+		running, jumping, climbing and navigating rough terrain, sometimes, even
+		chasing down people in different environments. Game Wardens are expected
+		to know everything about wildlife regulations to make sure people are
+		within the legal limits. You’re going to have to get a boating license,
+		practice water rescues, learn defensive tactics, drive many kinds of
+		4-wheel offroad vehicles, first aid, law enforcement, firearm training,
+		and again, you must be physically fit.
+	</p>
+	<p>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;What colleges would help
+		you become a Game Warden? The ones that <a
+			href="https://gamewarden.org"
+			target="_blank">gamewarden.org</a
+		> supply are the Grand Canyon University for Bachelors of Science in Justice
+		Studies and Masters of Science in Criminal Justice, Southern New Hampshire
+		University for a Bachelor of Science in Criminal Justice, Liberty University
+		for a Bachelors of Science In Criminal Justice, Strayer University for also
+		Bachelors of Science In Criminal Justice, and finally Rasmussen University
+		for Online Associates and Bachelors in Criminal Justice Leadership Management.
+	</p>
+	<img src="/image1.png" alt="Graph of jobs" />
+	<p>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Let’s take a look at
+		some statistics now. The Game Warden job is very stable, with only a
+		slight rise in 2018, otherwise, it’s very stable and not projected to
+		change much, this graph shows employment of Game Wardens for each year.
+	</p>
+	<img src="/image2.png" alt="Game Warden Popularity" />
+	<p>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This graph here shows
+		Game Warden popularity per state, as you can see here, the top states
+		for Game Wardens are Georgia, Florida, Texas, and Tennessee. Georgia, is
+		the state with the most Game Wardens however with 560 employed
+	</p>
+	<p>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;What are the fastest
+		growing states for Game Wardens? Well Utah comes in at top with a 175%
+		growth rate, next is Arizona with a 100% growth rate, Florida with 49%,
+		Nevada with 20%, Maine with 16%, and all the other states have stayed
+		relatively stable.
+	</p>
+	<p>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Want to learn more about
+		becoming a Game Warden? check out the video posted on this page to learn
+		How To Become a Game Warden in Wisconsin
+	</p>
+	<iframe
+		src="https://www.youtube-nocookie.com/embed/NwgDt_-tdMU"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+	/>
+	<p>
+		Works Cited
+		<br /><br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;All Criminal Justice Schools.
+		“Game Warden Career and Salary Guide | Find Education Programs.” All Criminal
+		Justice Schools, 2016, www.allcriminaljusticeschools.com/law-enforcement/how-to-become-a-game-warden/.
+		<br /><br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Department of Numbers. “Wisconsin
+		State Household Income | Department of Numbers.” Www.deptofnumbers.com, www.deptofnumbers.com/income/wisconsin/.
+		<br /><br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Indeed Editorial Team. “How
+		Much Does a Fish and Game Warden Make?” Indeed Career Guide, 26 Feb. 2021,
+		www.indeed.com/career-advice/pay-salary/how-much-do-fish-and-game-wardens-make.
+		<br /><br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;www.recruiter.com. “Career
+		Outlook and Job Vacancies for Fish and Game Wardens.” Recruiter, 2009, www.recruiter.com/careers/fish-and-game-wardens/outlook/.
+		<br /><br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;XYZ Media, Inc. “How to Become
+		a Game Warden | GameWarden.org.” GameWarden.org, www.gamewarden.org/how-to-become.
+		<br /><br />
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;XYZ Media, Inc. “Wisconsin
+		Game Warden | GameWarden.org.” GameWarden.org, www.gamewarden.org/state/wisconsin.
+		Accessed 17 May 2021.
+	</p>
 </Content>
+
+<style>
+	.title {
+		position: fixed;
+		top: 5px;
+		left: 5px;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+			Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+	}
+
+	.title h1 {
+		display: inline-block;
+		margin: 0;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+			Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+	}
+
+	iframe {
+		width: 100%;
+		height: 450px;
+	}
+</style>
